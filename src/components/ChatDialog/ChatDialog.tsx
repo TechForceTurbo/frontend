e175a4d1f6e4c7b91c4be089359720bf8bafb522
Vladimir Strugov <Strugov2001@yandex.ru>
2024-03-25 15:59:59 +0300

    fix errors

--- src/components/ChatDialog/ChatDialog.tsx
+++ src/components/ChatDialog/ChatDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './ChatDialog.module.css';
 import { Inter } from 'next/font/google';
 import { useDispatch, useSelector } from 'react-redux';
