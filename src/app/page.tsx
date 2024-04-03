6c4e13e1cc733dfc060aaefff9bbc86fe5b438ed
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-04-03 11:42:44 +0300

    feat: footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,19 @@ import { Provider } from 'react-redux'
 import store from '@/redux/store'
 import MainMenu from '@/components/MainMenu/MainMenu'
 import ChatDialog from '@/components/ChatDialog/ChatDialog'
-import Header from '../components/Header/Header'
+import Header from '@/components/Header/Header'
+import Footer from '@/components/Footer/Footer'
 
 export default function Home() {
   return (
     <Provider store={store}>
+      <Header />
       <main className={styles.main}>
-        <Header />
         <MainMenu />
         <MainButton />
         <ChatDialog />
       </main>
+      <Footer />
     </Provider>
   )
 }
