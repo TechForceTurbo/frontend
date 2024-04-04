e86f26d7617c3a0c968e33390c5b4bb95a27017c
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-04-04 23:57:06 +0300

    ref: img alt

--- src/components/Advertisement/Advertisement.tsx
+++ src/components/Advertisement/Advertisement.tsx
@@ -1,22 +1,22 @@
-import React, { FC } from 'react';
-import styles from './Advertisement.module.css';
+import React, { FC } from 'react'
+import styles from './Advertisement.module.css'
 
 const Advertisement: FC = () => {
   return (
     <section className={styles.section}>
       <div className={styles.imgContainer}>
         <div className={styles.bigImage}>
-          <img src="/images/Artboard-52.webp" alt="Мое изображение" />
+          <img src="/images/Artboard-52.webp" alt="пленки" />
         </div>
         <div className={styles.smallImage}>
-          <img src="/images/dippa_gw1.webp" alt="Мое изображение" />
+          <img src="/images/dippa_gw1.webp" alt="чернила" />
         </div>
         <div className={styles.smallImage}>
-          <img src="/images/smchemi1.webp" alt="Мое изображение" />
+          <img src="/images/smchemi1.webp" alt="скотч" />
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Advertisement;
+export default Advertisement
