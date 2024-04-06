afb2227fac0bf686ecd97b0f4c16d9d5142b9c51
Vladimir Strugov <Strugov2001@yandex.ru>
2024-04-06 15:54:30 +0300

    refactor

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,74 +1,77 @@
-import React, { FC, useEffect, useRef, useState } from 'react';
-import MessageElement from '../MessageElement/MessageElement';
-import styles from './Messages.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '@/redux/types';
-import Hints from '../Hints/Hints';
-import getHistoryMessages, { getPreviousMessages } from '@/utils/getHistoryMessages';
-import { addMessagesFromHistory, clearSetMessages } from '@/redux/reducers/setMessagesSlice';
-import TypingInformation from '../TypingInformation/TypingInformation';
-import { selectUnansweredMessageCount } from '@/redux/reducers/unansweredMessagesSlice';
+import React, { FC, useEffect, useRef, useState } from 'react'
+import MessageElement from '../MessageElement/MessageElement'
+import styles from './Messages.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '@/redux/types'
+import Hints from '../Hints/Hints'
+import getHistoryMessages, { getPreviousMessages } from '@/utils/getHistoryMessages'
+import { addMessagesFromHistory, clearSetMessages } from '@/redux/reducers/setMessagesSlice'
+import TypingInformation from '../TypingInformation/TypingInformation'
+import { selectUnansweredMessageCount } from '@/redux/reducers/unansweredMessagesSlice'
 
 const Messages: FC = () => {
-  const [nextPageLink, setNextPageLink] = useState<string | null>(null);
+  const [nextPageLink, setNextPageLink] = useState<string | null>(null)
 
-  const messages = useSelector((state: RootState) => state.setMessages?.items);
-  const isError = useSelector((state: RootState) => state.isErrorConnection.isError);
-  const textError = useSelector((state: RootState) => state.isErrorConnection.errorMessage);
-  const unansweredMessageCount = useSelector(selectUnansweredMessageCount);
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-  const messagesStartRef = useRef<HTMLDivElement>(null);
-  const dispatch = useDispatch();
+  const messages = useSelector((state: RootState) => state.setMessages?.items)
+  const isError = useSelector((state: RootState) => state.isErrorConnection.isError)
+  const textError = useSelector((state: RootState) => state.isErrorConnection.errorMessage)
+  const unansweredMessageCount = useSelector(selectUnansweredMessageCount)
+  const messagesEndRef = useRef<HTMLDivElement>(null)
+  const messagesStartRef = useRef<HTMLDivElement>(null)
+  const dispatch = useDispatch()
 
   const handleReceivedMessagesData = (data: any) => {
-    dispatch(addMessagesFromHistory(data.results));
-    setNextPageLink(data.next);
-  };
+    dispatch(addMessagesFromHistory(data.results))
+    if (data.next.includes('http://')) {
+      data.next = data.next.replace('http://', 'https://')
+    }
+    setNextPageLink(data.next)
+  }
 
   const handleScroll = () => {
     if (messagesStartRef.current?.scrollTop === 0 && nextPageLink !== null) {
       getPreviousMessages(nextPageLink)
         .then(handleReceivedMessagesData)
         .catch((error: Error) => {
-          console.error(error.message);
-        });
+          console.error(error.message)
+        })
     }
-  };
+  }
 
   useEffect(() => {
     getHistoryMessages()
       .then(handleReceivedMessagesData)
       .catch((error: Error) => {
-        console.error(error.message);
-      });
+        console.error(error.message)
+      })
 
     return () => {
-      dispatch(clearSetMessages());
-    };
-  }, [dispatch]);
+      dispatch(clearSetMessages())
+    }
+  }, [dispatch])
 
   useEffect(() => {
     if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
     }
-  }, [messages]);
+  }, [messages])
 
   useEffect(() => {
     if (messagesStartRef.current) {
-      messagesStartRef.current.addEventListener('scroll', handleScroll);
+      messagesStartRef.current.addEventListener('scroll', handleScroll)
     }
 
     return () => {
       if (messagesStartRef.current) {
-        messagesStartRef.current.removeEventListener('scroll', handleScroll);
+        messagesStartRef.current.removeEventListener('scroll', handleScroll)
       }
-    };
-  }, [nextPageLink]);
+    }
+  }, [nextPageLink])
 
   return (
     <div className={styles.box} ref={messagesStartRef}>
       {messages !== undefined && messages.length > 0 ? (
-        messages.map((message) => (
+        messages.map(message => (
           <MessageElement
             key={Math.random()}
             user={message.user}
@@ -88,7 +91,7 @@ const Messages: FC = () => {
       {unansweredMessageCount !== 0 && <TypingInformation />}
       <div ref={messagesEndRef} />
     </div>
-  );
-};
+  )
+}
 
-export default Messages;
+export default Messages
